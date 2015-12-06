76f9cfb780d5f36e5fbd6d8cec7c978a0e51cb94
autorun <rbmike@163.com>
2015-12-06 15:57:33 +0800

    remove no use code

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                 ; [clj-ta-lib "0.1.0"]
                  [com.taoensso/timbre "4.1.4"]
-                 [com.taoensso/carmine "2.12.0"]
                  [clj-time "0.11.0"]
                  [clj-http "2.0.0"]
                  [org.clojure/data.csv "0.1.3"]
